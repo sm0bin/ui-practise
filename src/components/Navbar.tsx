61d36905f756931a6ad9be7600ede01ec48de685
sm0bin <s1810677110@ru.ac.bd>
2025-08-01 15:59:59 +0600

    nav

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,19 @@
 import { NavLink } from "react-router-dom";
 import { links } from "../utls/Links";
 
+const style = {
+  active: {
+    color: "red",
+    background: "black",
+  },
+};
+
 const Navbar = () => (
   <nav className="flex gap-4">
     {links.map((l) => (
       <NavLink
-        className={`rounded-full bg-blue-300 p-2 hover:bg-blue-500 py-1`}
+        style={({ isActive }) => (isActive ? style.active : undefined)}
+        className={`rounded-full bg-blue-300 p-2 hover:bg-blue-500 py-1 `}
         key={l.name}
         to={l.path}
       >
